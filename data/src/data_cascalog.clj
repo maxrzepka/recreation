750e6593e14df787e5e42df479edbbdd3b08540c
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2012-02-22 22:12:01 +0100

    add more queries

--- data/src/data_cascalog.clj
+++ data/src/data_cascalog.clj
@@ -3,29 +3,21 @@
   (require [cascalog.api :as ca]
            [cascalog.ops :as co]))
 
-;; (simple-query  (hfs-textline "/tmp/results"))
+;; (
 (defn query1
-  "Returns results of a simple query
-   out = :dir result saved in folder by default /tmp/results"
-  [out & more]
-  (let [output ({:dir (ca/hfs-textline (if (seq more) (first more) "/tmp/results"))} out (ca/stdout))]
-       (ca/?<- output
-               ["?person" "?a2"]
-               (person "?person" "?gender" "?age")
-               (< "?age" 30)
-               (* 2 "?age" :> "?a2"))))
+   []
+   (ca/??<- ["?person" "?a2"]
+            (person "?person" "?gender" "?age")
+            (< "?age" 30)
+            (* 2 "?age" :> "?a2")))
 
-(comment
-  "Set up to run cascalog from emacs REPL : copy from cascalog.playgroung "
-  (import '[java.io PrintStream]
-          '[cascalog WriterOutputStream]
-          '[org.apache.log4j Level Logger WriterAppender SimpleLayout])
+(defn query2
+  "Returns person following more than n persons, uses !! for outer join"
+  [n]
+  (ca/??<- [?name ?count]
+           (person "?name" "?gender" "?age")
+           (follows "?name" "!!followed")
+           (co/!count !!followed :> ?count)
+           (> "?count" n)))
 
-  "TODO how to change log level"
-  (-> (Logger/getRootLogger)
-;      (.setLevel (. Level FATAL))
-      (.addAppender (WriterAppender. (SimpleLayout.) *out*)))
-;      (.setLevel (. Level DEBUG)))
-  (System/setOut (PrintStream. (WriterOutputStream. *out*)))
-)
 
--- data/src/data_congomongo.clj
+++ data/src/data_congomongo.clj
@@ -3,6 +3,8 @@
        [somnium.congomongo.config :only [*mongo-config*]])
   (require [somnium.congomongo :as m]))
 
+;;  mongodb daemon should run  
+;; 
 (defn bootstrap-mongo []
   (let [mongo-url "mongodb://localhost:27017/test"]
     (when (not (m/connection? *mongo-config*))
@@ -10,11 +12,24 @@
       (m/mongo! :db "test" :host "localhost" :port 27017 ))))
 
 (defn init-data
-  "Create a collection persons and insert person as  (no join in mongodb)"
+  "Creates a collection named persons and inserts one record per person (no join in mongodb)
+    {:name \"emily\" :gender \"f\" :age 25 :follows [\"gary\" \"george\" \"bob\" \"alice\"] }
+"
   []
-  (m/create-collection! :persons)
   (m/mass-insert! :persons person-follows))
 
-(defn simple-query []
-  (m/fetch :persons :where {:age 28}))
-           
+(defn query1
+  "Simple query with where and only clauses"
+  []
+  (m/fetch :persons :where {:age {:$gt 28}} :only [:name]))
+
+(defn query2
+  " db.persons.find({follows: {$size:  2 ,$in: [\"gary\"]}} ); "
+  []
+  (m/fetch :persons :where { :follows {:$size 2 :$in ["gary"]}}))
+
+;;
+;; How to get any person following more than 2 persons ?
+;; These 2 queries are incorrects
+;;   db.persons.find({$where: "this.follows.length > 2" } );
+;;    db.persons.find({follows: {$size:  {:$gt 2}}});
