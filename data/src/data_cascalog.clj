b77b75ec07f2d4d2e8cee071ebbf7f40be7b76b0
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2012-02-25 21:18:44 +0100

    added some questions

--- data/src/data_cascalog.clj
+++ data/src/data_cascalog.clj
@@ -3,21 +3,30 @@
   (require [cascalog.api :as ca]
            [cascalog.ops :as co]))
 
-;; (
-(defn query1
-   []
-   (ca/??<- ["?person" "?a2"]
-            (person "?person" "?gender" "?age")
-            (< "?age" 30)
-            (* 2 "?age" :> "?a2")))
+;; A query starts with an output [ ?person ?a2 ] and the rest are predicates
+;; A variable can be symbol ?a2 or string "?a2" , when starting with ! is nullable
+;; Types of predicates :
+;;   - generator ( source of data ) like (person "?person" "?gender" "?age")
+;;   - operation like (< "?age" 30) 
+;;   - aggregator like  (co/!count !!followed :> ?count)
+;;
+;; predicate without output variables (< ?age 30) act as a filter
 
-(defn query2
-  "Returns person following more than n persons, uses !! for outer join"
-  [n]
-  (ca/??<- [?name ?count]
+(defn person-younger-than
+   [age]
+   (ca/??<- ["?person" "?gender" "?age"]
+            (person "?person" "?gender" "?age")
+            (< "?age" age))
+
+(defn count-followed
+  "Returns name , age and count of persons followed"
+  []
+  (ca/??<- [?name ?age ?count]
            (person "?name" "?gender" "?age")
            (follows "?name" "!!followed")
-           (co/!count !!followed :> ?count)
-           (> "?count" n)))
+           (co/!count !!followed :> ?count)))
 
+
+;; TODO how to make it more dynamic ?
+;; describe the query as plain map (cf mongodb) and coerce map to a cascalog query
 
